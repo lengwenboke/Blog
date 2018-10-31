d7134088ae5a76b4ec6ab6560260f9a6cdedccb7
lengwen <lengwenboke@foxmail.com>
2018-10-31 16:26:05 +0800

    详情页展示，完成一半

--- blogpost/urls.py
+++ blogpost/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from .views import *
 
 app_name = 'blog'
-
 urlpatterns = [
     url(r'^$', IndexView.as_view(), name="index"),
+    url(r'^post/(?P<pk>[0-9]+)/$', PostDetailView.as_view(), name="detail")
 ]
--- blogpost/views.py
+++ blogpost/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from blogpost.models import *
 
@@ -11,3 +11,12 @@ class IndexView(ListView):
     template_name = 'blog/index.html'
     context_object_name = 'post_list'
 
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+
+    def get_object(self, queryset=None):
+        post = super(PostDetailView, self).get_object(queryset=queryset)
+        return post
